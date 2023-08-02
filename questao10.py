a8c17b95929435d336375a8bd004f55b2397ee9e
Felipe Barbosa <felipebarbosadosreis092@gmail.com>
2023-08-02 11:37:48 -0300

    segundo commit

--- questao10.py
+++ questao10.py
@@ -2,9 +2,9 @@
 Fazer um algoritmo que efetue o cálculo do valor de uma prestação em atraso, utilizando a fórmula prestação	=
 valor	+	(valor	x	(taxa	:	100)	x	tempo	em	dias)
 '''
-val = int(input("Qual o valor da prestação?"))
+val = float(input("Qual o valor da prestação?"))
 tax = float(input("Qual o valor da taxa?"))
-temp = int(input("Qual o tempo em atraso (dias) ?"))
+temp = float(input("Qual o tempo em atraso (dias) ?"))
 
 prest = val +(val*(tax/100)*temp)
 
