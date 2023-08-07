5e5651e80c88b88d03bdafb60e0885fa25ad7903
Felipe Barbosa <felipebarbosadosreis092@gmail.com>
2023-08-07 11:35:04 -0300

    commit informativo, para trabalhos v2

---
+++ operadores/recursos.py
@@ -0,0 +1,10 @@
+import math
+
+raiz =math.sqrt(16) #raiz quadrada de 16
+print("raiz", raiz)
+
+potencia = math.pow(3,4)
+print("a potencia é ", potencia)
+
+numero_pi = math.pi
+print("O valor do PI", numero_pi)
